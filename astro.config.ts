2503e1e64e4a8e5c927e29cceaba358b6f4e0a31
Vincentllxz <vincent.950313@gmail.com>
2026-05-29 00:22:56 +0800

    fix SEO

--- astro.config.ts
+++ astro.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
   trailingSlash: 'never',
+  build: { format: 'file' },
 
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
